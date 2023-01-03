3bf5a56d167d113cb901cba55d656e45e642ed49
Kazuya Izumi <shikibu9419@gmail.com>
2023-01-03 12:32:49 +0900

    Initial commit

---
+++ src/components/icons/logos.tsx
@@ -0,0 +1,15 @@
+export type PropsType = {
+  size: number;
+}
+
+export const Facebook = ({ size }: PropsType) => (
+  <img src="/icons/facebook.svg" width={size} height={size} />
+);
+
+export const Instagram = ({ size }: PropsType) => (
+  <img src="/icons/instagram.svg" width={size} height={size} />
+);
+
+export const Twitter = ({ size }: PropsType) => (
+  <img src="/icons/twitter.svg" width={size} height={size} />
+);
---
+++ src/routes/index.tsx
@@ -0,0 +1,39 @@
+import { component$, useStyles$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+import { Facebook, Instagram, Twitter } from '../components/icons/logos';
+import { Link } from '@builder.io/qwik-city';
+
+import styles from './styles.css?inline';
+
+export default component$(() => {
+  useStyles$(styles);
+
+  return (
+    <main>
+      <section class="title">
+        <h1 class="name">Kazuya Izumi</h1>
+        <p class="role">Engineer / Researcher / Artist</p>
+      </section>
+      <section class="links">
+        <Link href="https://twitter.com/shikibu9419" target="_blank">
+          <Twitter size={60} />
+        </Link>
+        <Link href="https://www.facebook.com/shikibu9419/" target="_blank">
+          <Facebook size={60} />
+        </Link>
+        <Link href="https://www.instagram.com/shikibu9419/" target="_blank">
+          <Instagram size={60} />
+        </Link>
+      </section>
+      <section>
+        <Link href="/">
+          See portfolio (WIP)
+        </Link>
+      </section>
+    </main>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'Welcome to my page',
+};
